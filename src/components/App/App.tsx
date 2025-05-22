c040538dc2cf083dcbda1e9553ee6788566ce66f
imyroslav <imyroslav@ukr.net>
2025-05-22 22:23:07 +0200

    created-componets-app-cafeinfo

---
+++ src/components/App/App.tsx
@@ -0,0 +1,17 @@
+
+import css from './App.module.css'
+import CafeInfo from '../CafeInfo/CafeInfo'
+
+function App() {
+
+  return (
+    <div className={css.app}>
+
+    <CafeInfo />
+
+    </div>
+
+  )
+}
+
+export default App
