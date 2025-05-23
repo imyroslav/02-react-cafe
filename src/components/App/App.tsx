b7d2605bc92074916434ca2f200f6ac5b780e1a4
imyroslav <imyroslav@ukr.net>
2025-05-23 23:19:00 +0200

    created-types

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,15 @@
+import { useState } from 'react';
 
 import css from './App.module.css'
 import CafeInfo from '../CafeInfo/CafeInfo'
 
+
+
 function App() {
+
+  const [votes, setVotes] = useState({});
+
+  const handleVote = 
 
   return (
     <div className={css.app}>
---
+++ src/types/votes.ts
@@ -0,0 +1,10 @@
+interface Votes {
+    good: number;
+    neutral: number;
+    bad: number;
+}
+
+type VoteType = "good" | "neutral" | "bad";
+
+
+export default Votes;
